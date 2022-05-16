b4a58ebe8e7e6b019fd4889a06e73d973041b610
efss7 <ericfelipess07@gmail.com>
2022-05-16 13:36:34 -0300

    routes e global state

---
+++ src/globalState/Context.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+
+export const FutureEats = React.createContext()
---
+++ src/routes/Router.js
@@ -0,0 +1,31 @@
+import React from "react";
+import CartPage from "../pages/CartPage/CartPage"
+import EstablishmentPage from "../pages/EstablishmentPage/EstablishmentPage"
+import FeedPage from "../pages/FeedPage/FeedPage"
+import HomePage from "../pages/HomePage/HomePage"
+import LoginPage from "../pages/LoginPage/LoginPage"
+import OrderPage from "../pages/OrderPage/OrderPage"
+import ProfilePage from "../pages/ProfilePage/ProfilePage"
+import SearchPage from "../pages/SearchPage/SearchPage"
+import SignUpPage from "../pages/SignUpPage/SignUpPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Router = () =>{
+    return(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/carrinho" element={<CartPage/>} />
+            <Route path="/estabelecimento" element={<EstablishmentPage/>} />
+            <Route path="/feed" element={<FeedPage/>} />
+            <Route path="/login" element={<LoginPage/>} />
+            <Route path="/ordem-pedido" element={<OrderPage/>} />
+            <Route path="/perfil" element={<ProfilePage/>} />
+            <Route path="/buscar-pedido" element={<SearchPage/>} />
+            <Route path="/cadastro" element={<SignUpPage/>} />
+        </Routes>
+    </BrowserRouter>
+    )
+}
+export default Router;
+
