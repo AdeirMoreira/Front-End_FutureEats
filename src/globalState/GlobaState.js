d00bb665f8c1b17d6062419ed382451fbc034c00
Adeir Maia <adeir.maia@hotmail.com>
2022-05-16 18:36:45 -0300

    iniciazação páginas singUP

--- src/globalState/GlobaState.js
+++ src/globalState/GlobaState.js
@@ -1,15 +1,21 @@
 import React from "react";
+import useForm from "../Hooks/useForm";
 import { FutureEats } from "./Context";
 
 
 
-export const GlobalState = (props) =>{
-    const params = { 
-        propiedade:"azul" 
+export const GlobalState = (props) => {
+    const dataForm = {
+        personalData: useForm({ name: "", email: "", cpf: "", password: "" }),
+        andressData: useForm({ street: "", number: "", neighbourhood: "", city: "", state: "", complement: "" })
     }
-    return(
+
+    const params = {
+        dataForm,
+    }
+    return (
         <FutureEats.Provider value={params}>
             {props.children}
-        </FutureEats.Provider> 
+        </FutureEats.Provider>
     )
 }
--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { FutureEats } from '../../globalState/Context'
 
 export default function SignUpPage() {
+  const params = useContext(FutureEats)
+
+  const preventDefault = (e) => {
+    e.preventDefault()
+    console.log(params.dataForm.personalData.form)
+  }
+
   return (
-    <div>SignUpPage</div>
+    <>
+      <h1>Future Eats</h1>
+      <form onSubmit={preventDefault}>
+        <label htmlFor='name'>Nome*
+          <input
+            id='name'
+            name='name' value={params.dataForm.personalData.form.name}
+            onChange={params.dataForm.personalData.onChange}
+            placeholder='Nome e Sobreome'
+            required />
+        </label>
+        <label htmlFor='email'>E-mail*
+          <input
+            id='email'
+            name='email' value={params.dataForm.personalData.form.email}
+            onChange={params.dataForm.personalData.onChange}
+            placeholder='email@email.com'
+            required />
+        </label>
+        <label htmlFor='cpf'>CPF*
+          <input
+            id='cpf'
+            name='cpf' value={params.dataForm.personalData.form.cpf}
+            onChange={params.dataForm.personalData.onChange}
+            placeholder='000.000.000-00'
+            required />
+        </label>
+        <label htmlFor=''>Senha*
+          <input
+            id='password'
+            name='password' value={params.dataForm.personalData.form.password}
+            onChange={params.dataForm.personalData.onChange}
+            placeholder='Mínimo 6 caracteres'
+            required />
+        </label>
+        {/* <label htmlFor='confirm'>Confirmar*
+          <input
+            id='confirm'
+            name='confirm' value={params.dataForm.personalData.form.password}
+            onChange={params.dataForm.personalData.onChange}
+            placeholder='Confirme a senha anterior'
+            required />
+        </label> */}
+        <button>enviar</button>
+      </form>
+    </>
+
+
   )
 }
