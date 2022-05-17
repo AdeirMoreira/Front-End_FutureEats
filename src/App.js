a76e4ce074cd5eab7f698c8aa6733ccb80b4dca5
Adeir Maia <adeir.maia@hotmail.com>
2022-05-17 09:33:51 -0300

    arruma o global

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Router  from './routes/Router';
-import { GlobalState } from './globalState/GlobaState';
+import Router from './routes/Router';
+import { GlobalState } from './globalState/GlobalState';
 
 function App() {
   return (
     <GlobalState>
-      <Router/>
+      <Router />
     </GlobalState>
   );
 }
