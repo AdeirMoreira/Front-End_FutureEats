237a95ba50ded5180bb8170f9ead5f3af25feb3c
Adeir Maia <adeir.maia@hotmail.com>
2022-05-18 19:26:07 -0300

    desquebrando

--- src/pages/EstablishmentPage/EstablishmentPage.js
+++ src/pages/EstablishmentPage/EstablishmentPage.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { FutureEats } from '../../globalState/Context'
-import { getRestaurants } from '../../services/FeedPage';
+
 import { getRestaurantDetails } from '../../services/RestaurantDetails'
 
 
@@ -9,34 +9,13 @@ export default function EstablishmentPage() {
   const params = useParams();
   const detail = useContext(FutureEats)
 
-
-
-
-  const token = localStorage.getItem('token');
-
   useEffect(() => {
-    getRestaurants(detail.rest, token)
     getRestaurantDetails(detail.setRestDetail, params.id)
   }, [])
-
-  // const restaurants = detail.rest && detail.rest.map((restaurant) => {
-  //   return (
-  //     <div key={restaurant.id}>
-  //       <img src={restaurant.logoUrl} alt="Logo restaurante" />
-  //       <h3>{restaurant.name}</h3>
-  //       <p>Tempo de entrega: {restaurant.deliveryTime}min</p>
-  //       <p>Frete R${restaurant.shipping}</p>
-  //     </div>
-  //   )
-  // })
-
-
-  
 
   const restDetail = detail.restDetail && detail.restDetail.restaurant.products.map((list) => {
     return (
       <div key={list.id}>
-
         <p>Nome: {list.name}</p>
         <p>Preço: {list.price}</p>
         <img src={list.photoUrl} />
@@ -49,7 +28,7 @@ export default function EstablishmentPage() {
   return (
     <div>
 
-    {/* <div>
+      {/* <div>
       <p>Selecione a quatidade desejada:</p>
       <select>
         <option>0</option>
@@ -70,10 +49,10 @@ export default function EstablishmentPage() {
 
       {detail.restDetail &&
         <>
-        <p>{detail.restDetail.restaurant.name}</p>
-        <p>{detail.restDetail.restaurant.category}</p>
-        <p>{detail.restDetail.restaurant.deliveryTime} minutos  Frete: R$ {detail.restDetail.restaurant.shipping}</p>
-        <p>{detail.restDetail.restaurant.address}</p>  
+          <p>{detail.restDetail.restaurant.name}</p>
+          <p>{detail.restDetail.restaurant.category}</p>
+          <p>{detail.restDetail.restaurant.deliveryTime} minutos  Frete: R$ {detail.restDetail.restaurant.shipping}</p>
+          <p>{detail.restDetail.restaurant.address}</p>
         </>
       }
       {restDetail}
