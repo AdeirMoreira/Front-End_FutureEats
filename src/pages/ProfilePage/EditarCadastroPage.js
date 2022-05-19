d5b3ba85bd2df1ba20bb8b88a3b74a870c9bb0ce
efss7 <ericfelipess07@gmail.com>
2022-05-19 15:52:58 -0300

    inicio da estilização do form de signup

--- src/pages/ProfilePage/EditarCadastroPage.js
+++ src/pages/ProfilePage/EditarCadastroPage.js
@@ -89,3 +89,4 @@ const EditarCadastroPage = () => {
 }
 
 export default EditarCadastroPage
+
--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -3,6 +3,13 @@ import { FutureEats } from '../../globalState/Context'
 import { SignUpRequest } from '../../services/SignUp'
 import { useInput } from '../../Hooks/useInput'
 import { useNavigate } from 'react-router-dom'
+import { InputsContainer, ScreenContainer } from '../SignUpPage/style'
+import { TextField } from '@material-ui/core'
+import { Button } from "@material-ui/core"
+import logo from "../../assets/Images/Logo.png"
+import Visibility from "@material-ui/icons/Visibility"
+import VisibilityOff from "@material-ui/icons/VisibilityOff"
+import InputAdornment from '@material-ui/core/InputAdornment'
 
 export default function SignUpPage() {
   const [confirm, handleValue] = useInput('')
@@ -21,61 +28,95 @@ export default function SignUpPage() {
 
   return (
     <>
-      <h1>Future Eats</h1>
+      <ScreenContainer>
+      <img src={logo}/>
+       <p>Cadastrar</p>
+      <InputsContainer>
       <form onSubmit={preventDefault}>
-        <label htmlFor='name'>Nome*
-          <input
-            id='name'
-            name='name' value={params.dataForm.personalData.form.name}
-            onChange={params.dataForm.personalData.onChange}
-            placeholder='Nome e Sobreome'
-            required />
-        </label>
-        <label htmlFor='email'>E-mail*
-          <input
-            id='email'
-            name='email' value={params.dataForm.personalData.form.email}
-            onChange={params.dataForm.personalData.onChange}
-            placeholder='email@email.com'
-            pattern='^[^\s@]+@[^\s@]+\.[^\s@]+$'
-            title='EX: email@provedor.com'
-            required />
-        </label>
-        <label htmlFor='cpf'>CPF*
-          <input
-            id='cpf'
-            name='cpf' value={params.dataForm.personalData.form.cpf}
-            onChange={params.dataForm.personalData.onChange}
-            placeholder='000.000.000-00'
-            pattern='^([0-9]){3}\.([0-9]){3}\.([0-9]){3}-([0-9]){2}$'
-            title='Use ponto e traço ao digitar o CPF EX: 000.000.000-00'
-            required />
-        </label>
-        <label htmlFor=''>Senha*
-          <input
-            id='password'
-            name='password' value={params.dataForm.personalData.form.password}
-            onChange={params.dataForm.personalData.onChange}
-            placeholder='Mínimo 6 caracteres'
-            pattern='^[a-zA-Z0-9]{6,}$'
-            title='Mínimo 6 caracteres entre letras e números'
-            required />
-        </label>
-        <div>
-          {confirmed && <p>A senha e a confirmação são diferentes</p>}
-        </div>
-        <label htmlFor='confirm'>Confirmar*
-          <input
-            id='confirm'
-            name='confirm' value={confirm}
-            onChange={handleValue}
-            placeholder='Confirme a senha anterior'
-            pattern='^[a-zA-Z0-9]{6,}$'
-            title='Mínimo 6 caracteres entre letras e números'
-            required />
-        </label>
-        <button>enviar</button>
+
+      <TextField element={<Button></Button>}
+      name={"name"}
+      value={params.dataForm.personalData.form.name}
+      onChange={params.dataForm.personalData.onChange}
+      label={"Nome"}
+      variant={"outlined"}
+      placeholder={"Nome e sobrenome"}
+      fullWidth
+      margin={"normal"}
+      required
+      />
+
+      <TextField
+      name={"email"}
+      value={params.dataForm.personalData.form.email}
+      onChange={params.dataForm.personalData.onChange}
+      label={"E-mail"}
+      variant={"outlined"}
+      inputProps={{pattern:'^[^\s@]+@[^\s@]+\.[^\s@]+$'}}
+      placeholder={"email@email.com"}
+      fullWidth
+      margin={"normal"}
+      required
+      />
+
+      <TextField
+      name={"cpf"}
+      value={params.dataForm.personalData.form.cpf}
+      onChange={params.dataForm.personalData.onChange}
+      label={"CPF"}
+      inputProps={{pattern:"([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})"}}
+      variant={"outlined"}
+      placeholder={"000.000.000-00"}
+      fullWidth
+      margin={"normal"}
+      required
+      />
+
+      <TextField
+      name={"password"}
+      value={params.dataForm.personalData.form.password}
+      onChange={params.dataForm.personalData.onChange}
+      label={"Senha"}
+      type={'password'}
+      variant={"outlined"}
+      placeholder={"Mínimo 6 caracteres entre letras e números"}
+      inputProps={{pattern:'^[a-zA-Z0-9]{6,}$'}}
+      fullWidth
+      margin={"normal"}
+      required
+      />
+      <>
+        {confirmed && <p>A senha e a confirmação são diferentes</p>}
+      </>
+      <TextField
+      name={'confirm'}
+      value={confirm}
+      onChange={handleValue}
+      type={'password'}
+      label={"Confirmar"}
+      placeholder={"Confirme a senha anterior"}
+      inputProps={{pattern:'^[a-zA-Z0-9]{6,}$'}}
+      variant={"outlined"}
+      fullWidth
+      margin={"normal"}
+      required
+      InputProps={{startAdornment: (
+        <Button edge="end">
+          <Visibility />
+        </Button>
+      ),}}
+      />
+      <Button
+      type={"submit"}
+      fullWidth
+      variant={'contained'}
+      color={'primary'}
+      margin={'normal'}
+      >Criar</Button>
+
       </form>
+      </InputsContainer>
+      </ScreenContainer>
     </>
   )
 }
