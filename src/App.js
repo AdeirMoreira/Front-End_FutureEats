2391a2f18a507e98df32424015080e29ed800f67
Araujod12 <davia9a@gmail.com>
2022-05-19 16:49:48 -0300

    splashScreen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect}from 'react';
 import Router from './routes/Router';
 import { GlobalState } from './globalState/GlobalState';
 import theme from './constants/themes';
 import { ThemeProvider } from '@material-ui/core/styles';
+import  styled  from 'styled-components';
+import splachScreen from './assets/Images/splachScreen.png'
+
+
+const SplachScreen = styled.div`
+width: 100vw;
+height: 100vh;
+background-image: url(${splachScreen});
+background-size: cover;
+background-repeat: no-repeat;
+
+`
 
 function App() {
+  const [loading,setLoading] = useState(true)
+  useEffect(()=>{
+  setTimeout(()=>{
+  setLoading(false)
+  },3000)
+  },[])
   return (
     <ThemeProvider theme={theme}>
     <GlobalState>
-      <Router />
+      {loading ? <SplachScreen/> : <Router />}
     </GlobalState>
     </ThemeProvider>
   );
